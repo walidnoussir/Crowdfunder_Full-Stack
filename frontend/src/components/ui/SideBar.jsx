d62e36f5d08de11f6e3a8b98e2dc53b969aaf363
walidnoussir <noussirw@gmail.com>
2026-05-20 10:43:08 +0100

    add the logout functionality

--- frontend/src/components/ui/SideBar.jsx
+++ frontend/src/components/ui/SideBar.jsx
@@ -1,8 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
 import { CalendarPlus, FolderKanban, House, LogOut } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/auth/authSlice";
 
 function SideBar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
   return (
     <div className="flex flex-col justify-between h-screen bg-slate-900 w-55 px-4 py-4 text-gray-400">
       <div className="space-y-8">
@@ -24,7 +34,7 @@ function SideBar() {
           </NavLink>
         </ul>
       </div>
-      <button className="flex items-center gap-2.5">
+      <button className="flex items-center gap-2.5" onClick={handleLogout}>
         <span>Logout</span>
         <LogOut size={20} />
       </button>
